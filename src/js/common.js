4f38562f80542f5847abd1ca5d76c755d1d127e7
Your Name <yang.lin.99@bk.ru>
2023-08-08 17:04:47 +0300

    Raw version

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  function validation(form) {
+    function removeError(input) {
+      const parent = input.parentNode;
+      if (input.classList.contains("error")) {
+        parent.querySelector(".error-label").remove();
+        input.classList.remove("error");
+      }
+    }
+
+    function createError(input, text) {
+      const parent = input.parentNode;
+      const errorLabel = document.createElement("label");
+      errorLabel.classList.add("error-label");
+      errorLabel.textContent = text;
+      input.classList.add("error");
+      parent.append(errorLabel);
+    }
+    let result = true;
+    const allInputs = form.querySelectorAll("input");
+
+    for (const input of allInputs) {
+      removeError(input);
+      if (input.value == "") {
+        console.log("Ошибка поля");
+        createError(input, "Поле не заполнено!");
+        result = false;
+      }
+    }
+    return result;
+  }
+
+  document
+    .getElementById("add-from")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (validation(this) == true) {
+        alert("Форма проверена успешно!");
+      }
+    });
+});
